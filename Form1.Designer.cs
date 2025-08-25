fc5526df080df8bb2acb8dbe76809d11204a5ae2
kimjihye058 <s2321@e-mirim.hs.kr>
2025-08-25 13:39:31 +0900

    #1 사용자 입력 폼 구현

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,16 +30,27 @@
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbWord = new System.Windows.Forms.TextBox();
+            this.tbWorth = new System.Windows.Forms.TextBox();
+            this.btnShowResult = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.btnShowResult);
+            this.groupBox1.Controls.Add(this.tbWorth);
+            this.groupBox1.Controls.Add(this.tbWord);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(342, 426);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
+            this.groupBox1.Text = "사용자 입력";
             this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
             // 
             // groupBox2
@@ -49,7 +60,49 @@
             this.groupBox2.Size = new System.Drawing.Size(428, 426);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "groupBox2";
+            this.groupBox2.Text = "결과";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(175, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "본인을 한 단어로 표현한다면? \r\n(예: 용감함, 신중함, 창의적 등)";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(287, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "당신이 가장 중요하게 생각하는 가치가 무엇인가요? \r\n(예: 사랑, 믿음, 성공 등)";
+            // 
+            // tbWord
+            // 
+            this.tbWord.Location = new System.Drawing.Point(20, 81);
+            this.tbWord.Name = "tbWord";
+            this.tbWord.Size = new System.Drawing.Size(100, 21);
+            this.tbWord.TabIndex = 2;
+            // 
+            // tbWorth
+            // 
+            this.tbWorth.Location = new System.Drawing.Point(20, 187);
+            this.tbWorth.Name = "tbWorth";
+            this.tbWorth.Size = new System.Drawing.Size(100, 21);
+            this.tbWorth.TabIndex = 3;
+            // 
+            // btnShowResult
+            // 
+            this.btnShowResult.Location = new System.Drawing.Point(46, 291);
+            this.btnShowResult.Name = "btnShowResult";
+            this.btnShowResult.Size = new System.Drawing.Size(237, 50);
+            this.btnShowResult.TabIndex = 4;
+            this.btnShowResult.Text = "결과보기";
+            this.btnShowResult.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -58,8 +111,12 @@
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.RightToLeftLayout = true;
+            this.Text = " ";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -68,6 +125,11 @@
 
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnShowResult;
+        private System.Windows.Forms.TextBox tbWorth;
+        private System.Windows.Forms.TextBox tbWord;
     }
 }
 
