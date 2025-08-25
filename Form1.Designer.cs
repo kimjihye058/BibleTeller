68cf24bd6742eb7f9d84ff98dc9373eb4238add6
kimjihye058 <s2321@e-mirim.hs.kr>
2025-08-25 14:00:16 +0900

    #1 메뉴 구현

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,8 +36,15 @@
             this.tbWorth = new System.Windows.Forms.TextBox();
             this.btnShowResult = new System.Windows.Forms.Button();
             this.tbResult = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.파일ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.도움말ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.상담내역불러오기ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.끝내기ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.포츈텔러정보ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -47,9 +54,9 @@
             this.groupBox1.Controls.Add(this.tbWord);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Location = new System.Drawing.Point(12, 39);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(342, 426);
+            this.groupBox1.Size = new System.Drawing.Size(342, 399);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "사용자 입력";
@@ -58,9 +65,9 @@
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.tbResult);
-            this.groupBox2.Location = new System.Drawing.Point(360, 12);
+            this.groupBox2.Location = new System.Drawing.Point(360, 39);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(428, 426);
+            this.groupBox2.Size = new System.Drawing.Size(428, 399);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "결과";
@@ -68,7 +75,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(18, 37);
+            this.label1.Location = new System.Drawing.Point(18, 30);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(175, 24);
             this.label1.TabIndex = 0;
@@ -78,7 +85,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(18, 145);
+            this.label2.Location = new System.Drawing.Point(18, 120);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(287, 24);
             this.label2.TabIndex = 1;
@@ -86,23 +93,23 @@
             // 
             // tbWord
             // 
-            this.tbWord.Location = new System.Drawing.Point(20, 81);
+            this.tbWord.Location = new System.Drawing.Point(20, 74);
             this.tbWord.Name = "tbWord";
             this.tbWord.Size = new System.Drawing.Size(100, 21);
             this.tbWord.TabIndex = 2;
             // 
             // tbWorth
             // 
-            this.tbWorth.Location = new System.Drawing.Point(20, 187);
+            this.tbWorth.Location = new System.Drawing.Point(20, 162);
             this.tbWorth.Name = "tbWorth";
             this.tbWorth.Size = new System.Drawing.Size(100, 21);
             this.tbWorth.TabIndex = 3;
             // 
             // btnShowResult
             // 
-            this.btnShowResult.Location = new System.Drawing.Point(46, 291);
+            this.btnShowResult.Location = new System.Drawing.Point(52, 203);
             this.btnShowResult.Name = "btnShowResult";
-            this.btnShowResult.Size = new System.Drawing.Size(237, 50);
+            this.btnShowResult.Size = new System.Drawing.Size(237, 44);
             this.btnShowResult.TabIndex = 4;
             this.btnShowResult.Text = "결과보기";
             this.btnShowResult.UseVisualStyleBackColor = true;
@@ -113,8 +120,57 @@
             this.tbResult.Multiline = true;
             this.tbResult.Name = "tbResult";
             this.tbResult.ReadOnly = true;
-            this.tbResult.Size = new System.Drawing.Size(388, 379);
+            this.tbResult.Size = new System.Drawing.Size(388, 355);
             this.tbResult.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.파일ToolStripMenuItem,
+            this.도움말ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 파일ToolStripMenuItem
+            // 
+            this.파일ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.상담내역불러오기ToolStripMenuItem,
+            this.끝내기ToolStripMenuItem});
+            this.파일ToolStripMenuItem.Name = "파일ToolStripMenuItem";
+            this.파일ToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.파일ToolStripMenuItem.Text = "파일";
+            // 
+            // 도움말ToolStripMenuItem
+            // 
+            this.도움말ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.포츈텔러정보ToolStripMenuItem});
+            this.도움말ToolStripMenuItem.Name = "도움말ToolStripMenuItem";
+            this.도움말ToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+            this.도움말ToolStripMenuItem.Text = "도움말";
+            // 
+            // 상담내역불러오기ToolStripMenuItem
+            // 
+            this.상담내역불러오기ToolStripMenuItem.Name = "상담내역불러오기ToolStripMenuItem";
+            this.상담내역불러오기ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.상담내역불러오기ToolStripMenuItem.Text = "이전 내역 불러오기";
+            this.상담내역불러오기ToolStripMenuItem.Click += new System.EventHandler(this.상담내역불러오기ToolStripMenuItem_Click);
+            // 
+            // 끝내기ToolStripMenuItem
+            // 
+            this.끝내기ToolStripMenuItem.Name = "끝내기ToolStripMenuItem";
+            this.끝내기ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.끝내기ToolStripMenuItem.Text = "끝내기";
+            this.끝내기ToolStripMenuItem.Click += new System.EventHandler(this.끝내기ToolStripMenuItem_Click);
+            // 
+            // 포츈텔러정보ToolStripMenuItem
+            // 
+            this.포츈텔러정보ToolStripMenuItem.Name = "포츈텔러정보ToolStripMenuItem";
+            this.포츈텔러정보ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.포츈텔러정보ToolStripMenuItem.Text = "포츈텔러 정보";
+            this.포츈텔러정보ToolStripMenuItem.Click += new System.EventHandler(this.포츈텔러정보ToolStripMenuItem_Click);
             // 
             // Form1
             // 
@@ -123,15 +179,21 @@
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.RightToLeftLayout = true;
-            this.Text = " ";
+            this.Text = "포츈텔러 3103 김지혜";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -145,6 +207,12 @@
         private System.Windows.Forms.TextBox tbWorth;
         private System.Windows.Forms.TextBox tbWord;
         private System.Windows.Forms.TextBox tbResult;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 파일ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 도움말ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 상담내역불러오기ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 끝내기ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 포츈텔러정보ToolStripMenuItem;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -26,5 +26,25 @@ namespace FortuneTeller
         {
 
         }
+
+        private void 상ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 상담내역불러오기ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 끝내기ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void 포츈텔러정보ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
